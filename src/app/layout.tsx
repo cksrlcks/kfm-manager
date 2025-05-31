ba706f2b4271c0bd8694338f1ed29806b428f48f
chanki.kim <chanki.kim89@gmail.com>
2025-05-31 22:05:19 +0900

    feat: add shadcn component (seperator, button)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
 import localFont from "next/font/local";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
 
 const pretendard = localFont({
   src: "../assets/fonts/PretendardVariable.woff2",
@@ -21,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${inter.variable} ${pretendard.className} antialiased`}>
-        {children}
-      </body>
+      <body className={`${pretendard.className} antialiased`}>{children}</body>
     </html>
   );
 }
