70dfbc3374f13032d3343514bcdc09b43dd58a68
chanki.kim <chanki.kim89@gmail.com>
2025-06-01 21:35:58 +0900

    feat: initialize drizzle and betterauth

---
+++ src/lib/auth.ts
@@ -0,0 +1,22 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { admin } from "better-auth/plugins";
+import { db } from "@/db";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    additionalFields: {
+      confirmed: {
+        type: "boolean",
+        default: false,
+      },
+    },
+  },
+  plugins: [admin()],
+});
