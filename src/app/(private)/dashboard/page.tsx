f71b6a85977a6efb7518b6ec24913e4d60b2ad55
chanki.kim <chanki.kim89@gmail.com>
2025-06-06 23:38:09 +0900

    feat: add dashboard page content

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,7 +1,81 @@
+import Link from "next/link";
+import {
+  ArrowUpRight,
+  CirclePlus,
+  DraftingCompass,
+  Globe,
+  Mail,
+  Menu,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { SITE_LISTS } from "@/constants";
 import { verifyAdminSession } from "@/lib/dal";
 
 export default async function DashboardPage() {
   await verifyAdminSession();
 
-  return <div>Dashboard Page</div>;
+  return (
+    <div className="mx-auto w-full max-w-xl">
+      <div className="space-y-5">
+        <h2 className="flex items-center gap-2 font-semibold">
+          <Menu size={16} />
+          자주사용하는 메뉴
+        </h2>
+        <div className="flex flex-wrap gap-1">
+          <Button asChild variant="outline">
+            <Link href="/quotation/create">
+              <CirclePlus />
+              새로운 견적 생성하기
+            </Link>
+          </Button>
+          <Button asChild variant="outline">
+            <Link href="/inquiry/kfmblower">
+              <Mail />
+              <b>KFM</b> Blower 문의 내역
+            </Link>
+          </Button>
+          <Button asChild variant="outline">
+            <Link href="/inquiry/kfmbusan">
+              <Mail />
+              <b>KFM</b> Busan 문의 내역
+            </Link>
+          </Button>
+          <Button asChild variant="outline">
+            <Link href="/blower">
+              <DraftingCompass />
+              블로워 선정표
+            </Link>
+          </Button>
+        </div>
+      </div>
+      <Separator className="my-10" />
+      <div className="space-y-5">
+        <h2 className="flex items-center gap-2 font-semibold">
+          <Globe size={16} />
+          자주이용하는 사이트
+        </h2>
+        <ul className="grid grid-cols-2 gap-3">
+          {SITE_LISTS.map((item) => (
+            <li key={item.label}>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={item.url}
+                className="hover:bg-accent hover:text-accent-foreground flex items-center justify-between gap-2 rounded-md border bg-transparent p-4 transition-colors"
+              >
+                <div className="w-[80%]">
+                  <div className="text-sm font-semibold">{item.label}</div>
+                  <div className="text-muted-foreground overflow-hidden text-xs overflow-ellipsis whitespace-nowrap">
+                    {item.url}
+                  </div>
+                </div>
+                <ArrowUpRight size={16} />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -50,3 +50,50 @@ export const INQUIRY_SUB_MENUS: SubNavItem[] = [
     path: "/inquiry/kfmbusan",
   },
 ];
+
+export const SITE_LISTS = [
+  {
+    label: "다음",
+    url: "https://www.daum.net/",
+  },
+  {
+    label: "한국유체기계 웹하드",
+    url: "http://kfmblower.iptime.org:5000/#/signin",
+  },
+  {
+    label: "한국유체기계 주문서",
+    url: "https://c-portal.ecount.com/",
+  },
+  {
+    label: "국세청",
+    url: "https://hometax.go.kr/",
+  },
+  {
+    label: "엔투비",
+    url: "https://mro.entob.com/",
+  },
+  {
+    label: "국민은행",
+    url: "https://www.kbstar.com/",
+  },
+  {
+    label: "기업은행",
+    url: "https://kiup.ibk.co.kr/",
+  },
+  {
+    label: "웹팩스",
+    url: "https://webfax.uplus.co.kr/",
+  },
+  {
+    label: "SGI서울보증",
+    url: "https://www.sgic.co.kr/",
+  },
+  {
+    label: "압력 단위",
+    url: "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=KPA&oquery=KPA&tqi=TG91pspVuE8ssbAZDSwssssstXs-057705",
+  },
+  {
+    label: "법원경매정보",
+    url: "https://www.courtauction.go.kr/pgj/index.on?w2xPath=/pgj/ui/pgj100/PGJ151F00.xml",
+  },
+];
