30dcb9845bcb6ea0ed6ccd27eb30bcc57d838622
chanki.kim <chanki.kim89@gmail.com>
2025-06-08 05:52:07 +0900

    feat: modify CopyTextButton components

--- src/features/quotation/components/CopyTextButton.tsx
+++ src/features/quotation/components/CopyTextButton.tsx
@@ -1,12 +1,44 @@
-import { MouseEvent, PropsWithChildren } from "react";
+import { MouseEvent, PropsWithChildren, useRef } from "react";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 
 export default function CopyTextButton({ children }: PropsWithChildren) {
+  const focusedElRef = useRef<HTMLInputElement | HTMLTextAreaElement | null>(
+    null,
+  );
+
+  const handleMouseDown = () => {
+    const element = document.activeElement;
+    if (
+      element instanceof HTMLInputElement ||
+      element instanceof HTMLTextAreaElement
+    ) {
+      focusedElRef.current = element;
+    } else {
+      focusedElRef.current = null;
+    }
+  };
+
   const handleCopy = async (e: MouseEvent) => {
     const content = e.currentTarget.textContent;
+    const currentElement = focusedElRef.current;
 
-    if (content) {
+    if (!content) return;
+
+    if (currentElement) {
+      const start = currentElement.selectionStart;
+      const end = currentElement.selectionEnd;
+      const value = currentElement.value;
+
+      if (start === null || end === null) return;
+
+      currentElement.value = value.slice(0, start) + content + value.slice(end);
+      currentElement.setSelectionRange(
+        start + content.length,
+        start + content.length,
+      );
+      currentElement.focus();
+    } else {
       try {
         await navigator.clipboard.writeText(content);
         toast.success("클립보드에 복사했습니다. 붙여넣기로 사용해주세요");
@@ -17,7 +49,12 @@ export default function CopyTextButton({ children }: PropsWithChildren) {
   };
 
   return (
-    <Button variant="secondary" type="button" onClick={handleCopy}>
+    <Button
+      variant="secondary"
+      type="button"
+      onClick={handleCopy}
+      onMouseDown={handleMouseDown}
+    >
       {children}
     </Button>
   );
