ac02537698e01c5618a0d1cfff50488a5ac893f0
chanki.kim <chanki.kim89@gmail.com>
2025-06-08 08:08:30 +0900

    feat: update layout with metadata and Open Graph settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import localFont from "next/font/local";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
@@ -8,6 +9,27 @@ const pretendard = localFont({
   weight: "45 920",
   variable: "--font-pretendard",
 });
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://kfm-manager.vercel.app"),
+  title: "KFM-Manager",
+  description: "KFM의 관리자를 위한 웹사이트입니다.",
+  openGraph: {
+    url: "https://kfm-manager.vercel.app",
+    siteName: "KFM-Manager",
+    title: "KFM-Manager",
+    description: "KFM의 관리자를 위한 웹사이트입니다.",
+    type: "website",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "KFM-Manager",
+      },
+    ],
+  },
+};
 
 export default function GlobalLayout({
   children,
